219038dc5124bc622a306028915c5b3080bf37b0
Andreas Nyström <andy.i.flaskan@gmail.com>
2021-09-23 10:15:40 +0200

    Classes created, Scene and Entity implemented, now onto step 15

---
+++ Platformer/Door.cs
@@ -0,0 +1,11 @@
+using System;
+using SFML.System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Platformer
+{
+    public class Door : Entity
+    {
+    }
+}
---
+++ Platformer/Entity.cs
@@ -0,0 +1,44 @@
+using System;
+using SFML.System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Platformer
+{
+    public class Entity
+    {
+        private readonly string textureName;
+        protected readonly Sprite sprite;
+        public bool Dead;
+
+        protected Entity(string textureName)
+        {
+            this.textureName = textureName;
+            sprite = new Sprite();
+        }
+
+        public Vector2f Position
+        {
+            get => sprite.Position;
+            set => sprite.Position = value;
+        }
+
+        public virtual FloatRect Bounds => sprite.GetGlobalBounds();
+
+        public virtual void Create(Scene scene)
+        {
+            sprite.Texture = scene.LoadTexture(textureName);
+        }
+
+        public virtual void Update(Scene scene, float deltaTime)
+        {
+
+        }
+
+        public virtual void Render(RenderTarget target)
+        {
+            target.Draw(sprite);
+        }
+        
+    }
+}
